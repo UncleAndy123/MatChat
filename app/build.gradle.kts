405a99bfb20a2166750f61cc6a4fed2e5e574d9d
UncleAndy123 <144160285+UncleAndy123@users.noreply.github.com>
2026-09-16 13:52:47 -0400

    Bump version to 0.1.6-M0
    Increment versionCode to 6 and update versionName to 0.1.6-M0 in app/build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = appId
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.5-M0"
+        versionCode = 6
+        versionName = "0.1.6-M0"
         testInstrumentationRunner = "org.matchat.client.HiltTestRunner"
     }
 
